6a72d84c3d5a60052dce8049d3c47fd5654a076f
Aakash Gogale <aakashgogale123@gmail.com>
2026-09-09 18:26:15 +0530

    Update view details option

--- Frontend/src/modules/Food/pages/user/orders/UserOrderDetails.jsx
+++ Frontend/src/modules/Food/pages/user/orders/UserOrderDetails.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react"
 import { useNavigate, useParams, useLocation } from "react-router-dom"
 import useAppBackNavigation from "@food/hooks/useAppBackNavigation"
 import {
